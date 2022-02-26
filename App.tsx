fb28d5a365fe0ddd49a0b503959571fd59541422
Francisco Huentecura Cabrera <f.huentecura.cabrera@gmail.com>
2022-02-26 09:58:23 -0300

    :sparkles: add game to main screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import ButtonGrid from "./src/components/buttonGrid/ButtonGrid";
+import letters from "./test-board-2.json";
+import dictionary from "./dictionary.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ButtonGrid letters={letters.board} validWords={dictionary.words}></ButtonGrid>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
